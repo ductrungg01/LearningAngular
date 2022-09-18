63a8b0e8e92ebc8d60f650061a29653710219006
UIT.19520324 <68211021+vuductrung1411@users.noreply.github.com>
2022-09-18 22:36:59 +0700

    pipe and custom a pipe

---
+++ src/app/on-sale.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'onSale'
+})
+export class OnSalePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): string {
+    console.log("my pipe: ", value);
+    if (value){
+      return 'Đang hạ giá!';
+    } else return '';
+  }
+
+}
