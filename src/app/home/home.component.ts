2bc375996b3f92353668d71ae5f67f3fc51bab25
UIT.19520324 <68211021+vuductrung1411@users.noreply.github.com>
2022-09-18 19:20:27 +0700

    *ngFor and ngClass

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Vu Duc Trung';
   public age = 15;
-  public traiCay = ['táo', 'nho', 'xoài', 'cam', 'quýt'];
+  public traiCay = [
+    {ten: 'Táo', gia: 1000, haGia: true},
+    {ten: 'Nho', gia: 2000, haGia: false},
+    {ten: 'Xoài', gia: -3000, haGia: true},
+    {ten: 'Bưởi', gia: 4000, haGia: false},
+    {ten: 'Cam', gia: -50.10, haGia: false},
+    {ten: 'Quýt', gia: 6000, haGia: true},
+  ];
 
   constructor() { }
 
